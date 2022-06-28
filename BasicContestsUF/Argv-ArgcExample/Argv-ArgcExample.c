22f897579ffe9d2a5dca413776cf7b7040c6b64b
Shydes <jugoka947@gmail.com>
2022-06-28 00:27:56 -0300

    Argv-Argc

---
+++ BasicContestsUF/Argv-ArgcExample/Argv-ArgcExample.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <string.h>
+
+void MainWithArgs(int argc, char** argv) {
+    int i = 0, j = 0;
+    float numArgv1 = 0, numArgv2 = 0;
+    if (argc != 4) {
+        printf("ERROR 1");
+        return;
+    }
+    for (i = 1; i < 4; i += 2) {
+        for (j = 0; j < strlen(argv[i]); j++) {
+            if (!(argv[i][j] == '-' || argv[i][j] == '.' || (argv[i][j] < 58 && argv[i][j] > 47))) {
+                printf("ERROR %d", i + 1);
+                return;
+            }
+        }
+    }
+    sscanf(argv[1], "%f", &numArgv1);
+    sscanf(argv[3], "%f", &numArgv2);
+    if (strcmp(argv[2], "+") == 0) {
+        printf("%.2f", numArgv1 + numArgv2);
+    }
+    else if (strcmp(argv[2], "-") == 0) {
+        printf("%.2f", numArgv1 - numArgv2);
+    }
+    else if (strcmp(argv[2], "*") == 0) {
+        printf("%.2f", numArgv1 * numArgv2);
+    }
+    else if (strcmp(argv[2], "/") == 0) {
+        if (numArgv2 == 0) return;
+        printf("%.2f", numArgv1 / numArgv2);
+    }
+    else if (strcmp(argv[2], "%") == 0) {
+        printf("%.2f", (numArgv1 * numArgv2) / 100);
+    }
+    else  printf("ERROR 3");
+    return;
+}
+
+int main()
+{
+    int argc;
+
+    scanf_s("%d", &argc);
+
+    for (int i = 0; i < argc; i++) {
+        char* argv_item = *(argv_temp + i);
+
+        *(argv + i) = argv_item;
+    }
+
+    MainWithArgs(argc, argc, argv);
+
+    return 0;
+}
